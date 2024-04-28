9c778278c9a3dec2f2c011388b820b9609579b88
Xanders <124665689+xsamtech@users.noreply.github.com>
2024-04-28 15:35:45 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Boongo</title>
 
         <!-- Favicon -->
         <link rel="shortcut icon" type="image/png" href="{{ asset('assets/img/logo.png') }}">
