31237dbf4d819af06c284e603401ed87922b95ae
xanderssamoth <124665689+xsamtech@users.noreply.github.com>
2024-06-10 15:11:16 +0100

    n

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -50,10 +50,6 @@
 
         <!-- ============ Custom CSS ============ -->
         <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style.custom.css') }}">
-        <style>
-            .header-logo .logo { margin-top: 10px; }
-            @media only screen and (max-width: 500px) { .header-logo .logo { margin-top: 10px; margin-bottom: 10px; } }
-        </style>
 @if (request()->has('app_id'))
         <style>
             .detect-webview { display: none;!important }
