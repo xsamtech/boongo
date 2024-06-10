e85e6c77353cf70db3b037cc7865cdc90a302ad6
xanderssamoth <124665689+xsamtech@users.noreply.github.com>
2024-06-10 21:30:08 +0100

    Update AdminController.php

--- app/Http/Controllers/Web/AdminController.php
+++ app/Http/Controllers/Web/AdminController.php
@@ -35,6 +35,9 @@ class AdminController extends Controller
         // Find status by name
         $relevant_status_name = 'Pertinente';
         $relevant_status = $this::$api_client_manager::call('GET', getApiURL() . '/status/search/fr/' . $relevant_status_name);
+        // Find type by name
+        $document_type_name = 'Document';
+        $document_type = $this::$api_client_manager::call('GET', getApiURL() . '/type/search/fr/' . $document_type_name);
         // User inputs
         $inputs = [
             'work_title' => $request->register_work_title,
@@ -45,12 +48,21 @@ class AdminController extends Controller
             'categories_ids' => $request->register_categories_ids,
         ];
         // Add an admin
-        $work = $this::$api_client_manager::call('POST', getApiURL() . '/user', $api_token, $inputs);
+        $work = $this::$api_client_manager::call('POST', getApiURL() . '/work', $api_token, $inputs);
 
-        if ($work->success) {
+        if ($work->success AND $relevant_status->success AND $document_type->success) {
             // Udpate avatar if it is changed
             if ($request->data_other_user != null) {
-                $this::$api_client_manager::call('PUT', getApiURL() . '/user/update_avatar_picture/' . $work->data->id, $api_token, [
+                $this::$api_client_manager::call('PUT', getApiURL() . '/work/add_image/' . $work->data->id, $api_token, [
+                    'work_id' => $work->data->id,
+                    'image_64' => $request->data_other_user
+                ]);
+            }
+
+            // Upload document
+            if ($request->register_document != null) {
+                $this::$api_client_manager::call('POST', getApiURL() . '/work/upload_files', $api_token, [
+                    'type_id' => $document_type->data->id,
                     'work_id' => $work->data->id,
                     'image_64' => $request->data_other_user
                 ]);
@@ -59,7 +71,7 @@ class AdminController extends Controller
             return Redirect::back()->with('success_message', __('notifications.registered_data'));
 
         } else {
-            $resp_error = $inputs['work_title'] . '~' . $inputs['work_content'] . '~' . $inputs['work_url'] . '~' . $work->message;
+            $resp_error = $inputs['work_title'] . '~' . $inputs['work_content'] . '~' . $inputs['work_url'] . '~' . (!empty($work->message) ? $work->message : (!empty($relevant_status->message) ? $relevant_status->message : (!empty($document_type->message) ? $document_type->message : 'ERROR')));
 
             return Redirect::back()->with('error_message', $resp_error);
         }
