e5666e06448de871172d9e3d4c5d36e41fbe082b
xanderssamoth <124665689+xsamtech@users.noreply.github.com>
2024-06-10 22:39:58 +0100

    Update File.php

--- app/Http/Resources/File.php
+++ app/Http/Resources/File.php
@@ -21,7 +21,7 @@ class File extends JsonResource
         return [
             'id' => $this->id,
             'file_name' => $this->file_name,
-            'file_url' => $this->file_url,
+            'file_url' => $this->file_url != null ? (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/public/storage/' . $this->file_url : (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/public/assets/img/cover.png',
             'type' => Type::make($this->type),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
