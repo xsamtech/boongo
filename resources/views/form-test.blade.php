36ec48646b2beca65e00a17567e01a0ffac3f1a3
xanderssamoth <124665689+xsamtech@users.noreply.github.com>
2024-06-11 03:34:19 +0100

    Update form-test.blade.php

--- resources/views/form-test.blade.php
+++ resources/views/form-test.blade.php
@@ -138,91 +138,91 @@
         <!-- ALERT END-->
 @endif
 
-		<!-- SECTION -->
-		<form class="section py-5" method="POST" action="{{ route('admin.work.home') }}" enctype="multipart/form-data">
-            <h1 class="text-center mb-4">@lang('miscellaneous.admin.work.add')</h1>
+        <div class="py-5">
+            <form action="{{ route('admin.work.home') }}" method="post" enctype="multipart/form-data">
+                <h1 class="text-center mb-4">@lang('miscellaneous.admin.work.add')</h1>
 @csrf
 
-            <!-- container -->
-			<div class="container">
-				<!-- row -->
-				<div class="row">
-                    <div class="col-lg-5 col-sm-6 ms-auto">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="form-group mb-3">
-                                    <label for="register_work_title" class="visually-hidden">@lang('miscellaneous.admin.work.data.work_title')</label>
-                                    <input type="text" name="register_work_title" id="register_work_title" class="form-control" placeholder="@lang('miscellaneous.admin.work.data.work_title')" autofocus>
-                                </div>
+                <!-- container -->
+                <div class="container">
+                    <!-- row -->
+                    <div class="row">
+                        <div class="col-lg-5 col-sm-6 ms-auto">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="form-group mb-3">
+                                        <label for="register_work_title" class="visually-hidden">@lang('miscellaneous.admin.work.data.work_title')</label>
+                                        <input type="text" name="register_work_title" id="register_work_title" class="form-control" placeholder="@lang('miscellaneous.admin.work.data.work_title')" autofocus>
+                                    </div>
 
-                                <div class="form-group mb-3">
-                                    <label for="register_work_url" class="visually-hidden">@lang('miscellaneous.admin.work.data.work_url')</label>
-                                    <input type="text" name="register_work_url" id="register_work_url" class="form-control" placeholder="@lang('miscellaneous.admin.work.data.work_url')">
-                                </div>
+                                    <div class="form-group mb-3">
+                                        <label for="register_work_url" class="visually-hidden">@lang('miscellaneous.admin.work.data.work_url')</label>
+                                        <input type="text" name="register_work_url" id="register_work_url" class="form-control" placeholder="@lang('miscellaneous.admin.work.data.work_url')">
+                                    </div>
 
-                                <div id="editor" class="form-group mb-3">
-                                    <label for="register_work_content" class="visually-hidden">@lang('miscellaneous.admin.work.data.work_content')</label>
-                                    <textarea name="register_work_content" id="edit" class="form-control" placeholder="@lang('miscellaneous.admin.work.data.work_content')"></textarea>
-                                </div>
+                                    <div id="editor" class="form-group mb-3">
+                                        <label for="register_work_content" class="visually-hidden">@lang('miscellaneous.admin.work.data.work_content')</label>
+                                        <textarea name="register_work_content" id="edit" class="form-control" placeholder="@lang('miscellaneous.admin.work.data.work_content')"></textarea>
+                                    </div>
 
-                                <div class="form-group mb-3">
-                                    <label for="register_document">@lang('miscellaneous.upload.upload_document')</label>
-                                    <input type="file" name="register_document" id="register_document" class="form-control">
-                                </div>
+                                    <div class="form-group mb-3">
+                                        <label for="register_document">@lang('miscellaneous.upload.upload_document')</label>
+                                        <input type="file" name="register_document" id="register_document" class="form-control">
+                                    </div>
 
-                                <div class="form-group mb-3">
-                                    <label for="register_work_url">@lang('miscellaneous.menu.admin.group.type')</label>
-                                    <select name="type_id" class="form-select" aria-label="@lang('miscellaneous.admin.work.data.choose_type')">
-                                        <option class="small" selected disabled>@lang('miscellaneous.admin.work.data.choose_type')</option>
+                                    <div class="form-group mb-3">
+                                        <label for="register_work_url">@lang('miscellaneous.menu.admin.group.type')</label>
+                                        <select name="type_id" class="form-select" aria-label="@lang('miscellaneous.admin.work.data.choose_type')">
+                                            <option class="small" selected disabled>@lang('miscellaneous.admin.work.data.choose_type')</option>
 @forelse ($types as $type)
-                                        <option value="{{ $type->id }}">{{ $type->type_name }}</option>
+                                            <option value="{{ $type->id }}">{{ $type->type_name }}</option>
 @empty
 @endforelse
-                                    </select>
-                                </div>
+                                        </select>
+                                    </div>
 
-                                <div class="form-group mb-4">
-                                    <label class="d-block text-center">@lang('miscellaneous.admin.work.data.choose_categories')</label>
+                                    <div class="form-group mb-4">
+                                        <label class="d-block text-center">@lang('miscellaneous.admin.work.data.choose_categories')</label>
 @forelse ($categories as $category)
-                                    <div class="form-check mx-3">
-                                        <input type="checkbox" name="register_categories_ids[]" class="form-check-input" value="{{ $category->id }}" id="category_{{ $category->id }}">
-                                        <label class="form-check-label bng-text-secondary" for="category_{{ $category->id }}">{{ $category->category_name }}</label>
-                                    </div>
+                                        <div class="form-check mx-3">
+                                            <input type="checkbox" name="register_categories_ids[]" class="form-check-input" value="{{ $category->id }}" id="category_{{ $category->id }}">
+                                            <label class="form-check-label bng-text-secondary" for="category_{{ $category->id }}">{{ $category->category_name }}</label>
+                                        </div>
 @empty
 @endforelse
-                                </div>
-
-                                <button type="submit" class="btn btn-block btn-primary">@lang('miscellaneous.register')</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-6 me-auto">
-                        <div class="card rounded-4">
-                            <div id="otherUserImageWrapper" class="card-body pb-4 text-center">
-                                <p class="card-text m-0">@lang('miscellaneous.account.personal_infos.click_to_change_picture')</p>
-
-                                <div class="bg-image hover-overlay mt-3">
-                                    <img src="{{ asset('assets/img/cover.png') }}" alt="@lang('miscellaneous.admin.work.data.work_title')" class="other-user-image img-fluid rounded-4">
-                                    <div class="mask rounded-4" style="background-color: rgba(5, 5, 5, 0.5);">
-                                        <label role="button" for="image_other_user" class="d-flex h-100 justify-content-center align-items-center">
-                                            <i class="fa-solid fa-pencil-alt text-white fs-2"></i>
-                                            <input type="file" name="image_other_user" id="image_other_user" class="d-none">
-                                        </label>
-                                        <input type="hidden" name="data_other_user" id="data_other_user">
                                     </div>
-                                </div>
 
-                                <p class="d-none mt-2 mb-0 small text-success fst-italic">@lang('miscellaneous.waiting_register')</p>
+                                    <button type="submit" class="btn btn-block btn-primary">@lang('miscellaneous.register')</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-sm-6 me-auto">
+                            <div class="card rounded-4">
+                                <div id="otherUserImageWrapper" class="card-body pb-4 text-center">
+                                    <p class="card-text m-0">@lang('miscellaneous.account.personal_infos.click_to_change_picture')</p>
+
+                                    <div class="bg-image hover-overlay mt-3">
+                                        <img src="{{ asset('assets/img/cover.png') }}" alt="@lang('miscellaneous.admin.work.data.work_title')" class="other-user-image img-fluid rounded-4">
+                                        <div class="mask rounded-4" style="background-color: rgba(5, 5, 5, 0.5);">
+                                            <label role="button" for="image_other_user" class="d-flex h-100 justify-content-center align-items-center">
+                                                <i class="fa-solid fa-pencil-alt text-white fs-2"></i>
+                                                <input type="file" name="image_other_user" id="image_other_user" class="d-none">
+                                            </label>
+                                            <input type="hidden" name="data_other_user" id="data_other_user">
+                                        </div>
+                                    </div>
+
+                                    <p class="d-none mt-2 mb-0 small text-success fst-italic">@lang('miscellaneous.waiting_register')</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <!-- /row -->
                 </div>
-				<!-- /row -->
-			</div>
-			<!-- /container -->
-		</form>
-		<!-- /SECTION -->
+                <!-- /container -->
+            </form>
+        </div>
 
         <span id="btnBackTop" class="btn btn-floating btn-primary pb-0 d-none" style="position: fixed; bottom: 2rem; right: 2rem;"><i class="fa-solid fa-chevron-up"></i></span>
 
