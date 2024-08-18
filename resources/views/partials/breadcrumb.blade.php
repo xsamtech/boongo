827ee82d960c3c9a90b86f90803d916ce763de6f
xsamtech <124665689+xsamtech@users.noreply.github.com>
2024-08-18 04:09:18 +0100

    Update breadcrumb.blade.php

--- resources/views/partials/breadcrumb.blade.php
+++ resources/views/partials/breadcrumb.blade.php
@@ -1,6 +1,6 @@
 
 		<!-- BREADCRUMB -->
-		<div id="breadcrumb" class="section">
+		<div id="breadcrumb" class="section detect-webview">
 			<!-- container -->
 			<div class="container">
 				<!-- row -->
@@ -32,15 +32,15 @@
 
 @if (Route::is('about.home'))
                         <ul class="breadcrumb-tree">
-							<li class="detect-webview"><a href="{{ route('home') }}">@lang('miscellaneous.menu.home')</a></li>
-							<li class="active detect-webview">@lang('miscellaneous.menu.about')</li>
+							<li><a href="{{ route('home') }}">@lang('miscellaneous.menu.home')</a></li>
+							<li class="active">@lang('miscellaneous.menu.about')</li>
 						</ul>
 @endif
 @if (Route::is('about.entity'))
                         <ul class="breadcrumb-tree">
-							<li class="detect-webview"><a href="{{ route('home') }}">@lang('miscellaneous.menu.home')</a></li>
-							<li class="detect-webview"><a href="{{ route('about.home') }}">@lang('miscellaneous.menu.about')</a></li>
-							<li class="active detect-webview">{{ $entity_menu }}</li>
+							<li><a href="{{ route('home') }}">@lang('miscellaneous.menu.home')</a></li>
+							<li><a href="{{ route('about.home') }}">@lang('miscellaneous.menu.about')</a></li>
+							<li class="active">{{ $entity_menu }}</li>
 						</ul>
 @endif
 @if (Route::is('book.home'))
