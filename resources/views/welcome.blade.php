0ae7e93479275e68bbca96495204b25cb6c42113
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-04-30 12:43:20 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.guest')
+{{-- @extends('layouts.guest')
 
 @section('guest-content')
 
@@ -15,4 +15,22 @@
 		</div>
 		<!-- /SECTION -->
 
-@endsection
+@endsection --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Boongo</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 d-flex align-items-center" style="height: 50rem;">
+				<h1 class="h1 display-1 text-center text-uppercase">Bibliothèque numérique</h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
