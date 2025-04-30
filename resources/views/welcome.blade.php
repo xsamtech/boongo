81f69917e0e03fff571275a91bb2d051528cf2d7
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-04-30 13:20:31 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
 		/* Sticky footer styles 
 		-------------------------------------------------- */
 		html { position: relative; min-height: 100%; }
-		body { margin-bottom: 60px; /* Margin bottom by footer height */ }
+		body { font-family: Georgia, 'Times New Roman', Times, serif; margin-bottom: 60px; /* Margin bottom by footer height */ }
 		.footer { position: absolute; bottom: 0; width: 100%; 
 					height: 60px; /* Set the fixed height of the footer here */
 					line-height: 60px; /* Vertically center the text there */
@@ -45,30 +45,38 @@
 </head>
 <body>
 	<main role="main" class="container-fluid">
-		<h1 class="mt-5 text-center">Annonce du lancement d’une Bibliothèque Numérique au standard industriel</h1>
-		<p>Nous avons le plaisir de vous annoncer le lancement officiel de notre <strong>bibliothèque numérique de nouvelle génération, conçue selon les standards industriels les plus avancés. Cette plateforme innovante a pour vocation de rendre accessibles, à un large public, des contenus de haute valeur documentaire dans un objectif de <span class="text-danger">vulgarisation des données</span></strong>.</p>
-		<p>Notre bibliothèque propose un vaste catalogue comprenant :</p>
-		<ul class="list-group-flush">
-			<li class="list-group-item"><strong>Ouvrages</strong> de référence et de culture générale</li>
-			<li class="list-group-item"><strong>Médias</strong> audios, vidéo et photos</li>
-			<li class="list-group-item"><strong>Journaux</strong> historiques et contemporains</li>
-			<li class="list-group-item"><strong>Magazines</strong> spécialisés et grand public</li>
-			<li class="list-group-item"><strong>Revues</strong> scientifiques et techniques</li>
-			<li class="list-group-item"><strong>Cartes</strong> géographiques et thématiques</li>
-			<li class="list-group-item">Et bien d’autres ressources encore</li>
-		</ul>
-		<p>Grâce à une interface intuitive, une recherche optimisée et une compatibilité multi-supports, cette bibliothèque numérique s’adresse aussi bien aux chercheurs, enseignants, étudiants, qu’aux curieux désireux d’approfondir leurs connaissances.</p>
-		<p>Nous vous invitons à découvrir dès maintenant cet espace de savoir, pensé pour démocratiser l’accès à l’information et valoriser la richesse documentaire à travers un traitement numérique moderne et respectueux des normes internationales.</p>
-		<p>Rejoignez-nous pour faire de la connaissance un bien partagé.</p>
-		<hr class="my-3">
-		<p><u>Contact</u> : <strong>+243 855 273 394</strong></p>
-		<p><u>E-mail</u> : <strong><a href="mailto:contact@boongo7.com">contact@boongo7.com</a></strong></p>
-		<p><u>Adresse Physique</u> : <strong>Silikin Village - Concession COTEX / 63 Avenue Colonel Mondjiba - Kinshasa/Gombe</strong></p>
+		<div class="row">
+			<div class="col-lg-8 mx-auto">
+				<div class="view text-center">
+					<img src="{{ asset('assets/img/brand-reborn.png') }}" alt="" class="img-fluid">
+					<div class="mask"></div>
+				</div>
+				<h1 class="mt-5 text-center">Annonce du lancement d’une Bibliothèque Numérique au standard industriel</h1>
+				<p style="text-align: justify;">Nous avons le plaisir de vous annoncer le lancement officiel de notre <strong>bibliothèque numérique de nouvelle génération, conçue selon les standards industriels les plus avancés. Cette plateforme innovante a pour vocation de rendre accessibles, à un large public, des contenus de haute valeur documentaire dans un objectif de <span class="text-danger">vulgarisation des données</span></strong>.</p>
+				<p style="text-align: justify;">Notre bibliothèque propose un vaste catalogue comprenant :</p>
+				<ul class="list-group-flush">
+					<li class="list-group-item"><strong>Ouvrages</strong> de référence et de culture générale</li>
+					<li class="list-group-item"><strong>Médias</strong> audios, vidéo et photos</li>
+					<li class="list-group-item"><strong>Journaux</strong> historiques et contemporains</li>
+					<li class="list-group-item"><strong>Magazines</strong> spécialisés et grand public</li>
+					<li class="list-group-item"><strong>Revues</strong> scientifiques et techniques</li>
+					<li class="list-group-item"><strong>Cartes</strong> géographiques et thématiques</li>
+					<li class="list-group-item">Et bien d’autres ressources encore</li>
+				</ul>
+				<p style="text-align: justify;">Grâce à une interface intuitive, une recherche optimisée et une compatibilité multi-supports, cette bibliothèque numérique s’adresse aussi bien aux chercheurs, enseignants, étudiants, qu’aux curieux désireux d’approfondir leurs connaissances.</p>
+				<p style="text-align: justify;">Nous vous invitons à découvrir dès maintenant cet espace de savoir, pensé pour démocratiser l’accès à l’information et valoriser la richesse documentaire à travers un traitement numérique moderne et respectueux des normes internationales.</p>
+				<p style="text-align: justify;">Rejoignez-nous pour faire de la connaissance un bien partagé.</p>
+				<hr class="my-3">
+				<p><u>Contact</u> : <strong>+243 855 273 394</strong></p>
+				<p><u>E-mail</u> : <strong><a href="mailto:contact@boongo7.com">contact@boongo7.com</a></strong></p>
+				<p><u>Adresse Physique</u> : <strong>Silikin Village - Concession COTEX / 63 Avenue Colonel Mondjiba - Kinshasa/Gombe</strong></p>
+			</div>
+		</div>
 	</main>
 
 	<footer class="footer">
 		<div class="container">
-			<span class="d-inline-block me-5 text-muted">&copy; {{ date('Y') }} Reborn Tous droits réservés.</span> <span class="d-inline-block text-muted">Designed by <a href="https://xsamtech.com">Xsam Technologies</a></span>
+			<span class="d-inline-block mr-5 text-muted">&copy; {{ date('Y') }} Reborn Tous droits réservés.</span> <span class="d-inline-block text-muted">Designed by <a href="https://xsamtech.com">Xsam Technologies</a></span>
 		</div>
 	</footer>
 </body>
