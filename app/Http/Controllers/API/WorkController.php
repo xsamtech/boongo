3f49690682865821afd8cd22b59f80693eaeaad3
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-05-05 12:51:46 +0100

    Update WorkController.php

--- app/Http/Controllers/API/WorkController.php
+++ app/Http/Controllers/API/WorkController.php
@@ -632,23 +632,37 @@ class WorkController extends BaseController
                 $q->whereIn('categories.id', $request->categories_ids);
             })->orderByDesc('works.created_at');
 
+            // Add dynamic conditions
+            $query->when($request->type_id, function ($query) use ($request) {
+                return $query->where('type_id', $request->type_id);
+            });
+
+            $query->when($request->status_id, function ($query) use ($request) {
+                return $query->where('status_id', $request->status_id);
+            });
+
+            $works = $query->paginate(12);
+            $count_all = $query->count();
+
+            return $this->handleResponse(ResourcesWork::collection($works), __('notifications.find_all_works_success'), $works->lastPage(), $count_all);
+
         } else {
             $query->orderByDesc('works.created_at');
+
+            // Add dynamic conditions
+            $query->when($request->type_id, function ($query) use ($request) {
+                return $query->where('type_id', $request->type_id);
+            });
+
+            $query->when($request->status_id, function ($query) use ($request) {
+                return $query->where('status_id', $request->status_id);
+            });
+
+            $works = $query->paginate(12);
+            $count_all = $query->count();
+
+            return $this->handleResponse(ResourcesWork::collection($works), __('notifications.find_all_works_success'), $works->lastPage(), $count_all);
         }
-
-        // Add dynamic conditions
-        $query->when($request->type_id, function ($query) use ($request) {
-            return $query->where('type_id', $request->type_id);
-        });
-
-        $query->when($request->status_id, function ($query) use ($request) {
-            return $query->where('status_id', $request->status_id);
-        });
-
-        $works = $query->paginate(12);
-        $count_all = $query->count();
-
-        return $this->handleResponse(ResourcesWork::collection($works), __('notifications.find_all_works_success'), $works->lastPage(), $count_all);
     }
 
     /**
