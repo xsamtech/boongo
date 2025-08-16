1eb792ca67d491cc53a48e55c6080cb9136f7f05
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-08-16 18:10:01 +0100

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1035,11 +1035,13 @@ class UserController extends BaseController
             // If user is subscribed, send only data of the same category as that in the subscription
             if ($is_subscribed) {
                 $valid_subscription = $logged_in_user->validSubscriptions()->sortByDesc(function ($subscription) { return $subscription->pivot->created_at; })->first();
-                $partner = Partner::whereHas('categories')->exists() ? Partner::whereHas('categories', function ($query) use ($valid_subscription, $active_status) {
-                                        $query->where('id', $valid_subscription->category_id)->wherePivot('status_id', $active_status->id);
-                                    })->where(function ($query) use ($users_ids) {
-                                        $query->whereIn('from_user_id', $users_ids)->orWhereNotNull('from_organization_id');
-                                    })->inRandomOrder()->first() : null;
+
+                return $valid_subscription;
+                // $partner = Partner::whereHas('categories')->exists() ? Partner::whereHas('categories', function ($query) use ($valid_subscription, $active_status) {
+                //                         $query->where('id', $valid_subscription->category_id)->wherePivot('status_id', $active_status->id);
+                //                     })->where(function ($query) use ($users_ids) {
+                //                         $query->whereIn('from_user_id', $users_ids)->orWhereNotNull('from_organization_id');
+                //                     })->inRandomOrder()->first() : null;
 
             // Otherwise, send all data
             } else {
