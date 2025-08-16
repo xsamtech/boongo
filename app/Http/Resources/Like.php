a6b94aa871a3b7837d68d2fce0819b0422de3a83
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-08-16 03:34:28 +0100

    n

--- app/Http/Resources/Like.php
+++ app/Http/Resources/Like.php
@@ -11,23 +11,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
  */
 class Like extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
-            'user' => User::make($this->user),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
             'created_at_explicit' => $this->created_at->format('Y') == date('Y') ? explicitDayMonth($this->created_at->format('Y-m-d H:i:s')) : explicitDate($this->created_at->format('Y-m-d H:i:s')),
             'updated_at_explicit' => $this->updated_at->format('Y') == date('Y') ? explicitDayMonth($this->updated_at->format('Y-m-d H:i:s')) : explicitDate($this->updated_at->format('Y-m-d H:i:s')),
             'created_at_ago' => timeAgo($this->created_at->format('Y-m-d H:i:s')),
             'updated_at_ago' => timeAgo($this->updated_at->format('Y-m-d H:i:s')),
+            'user_id' => $this->user_id,
             'for_work_id' => $this->for_work_id,
             'for_message_id' => $this->for_message_id
         ];
