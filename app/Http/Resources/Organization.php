28bd25acd29b39d4bbc74b18d0bd73c1713f9c0d
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-08-19 03:34:12 +0100

    Update Organization.php

--- app/Http/Resources/Organization.php
+++ app/Http/Resources/Organization.php
@@ -34,7 +34,7 @@ class Organization extends JsonResource
             'type' => Type::make($this->type),
             'status' => Status::make($this->status),
             'programs' => Program::collection($this->programs),
-            'cover_url' => $this->cover_url != null ? getWebURL() . '/public/storage/' . $this->cover_url : getWebURL() . '/assets/img/banner-organization.png',
+            'cover_url' => trim($this->cover_url) != null ? getWebURL() . '/public/storage/' . $this->cover_url : getWebURL() . '/assets/img/banner-organization.png',
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
             'type_id' => $this->type_id,
