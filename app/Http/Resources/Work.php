2830dcc035e975c9512c00e75539207e9905bfeb
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-09-11 08:09:34 +0100

    Update Work.php

--- app/Http/Resources/Work.php
+++ app/Http/Resources/Work.php
@@ -80,7 +80,7 @@ class Work extends JsonResource
             'organization_owner' => Organization::make($this->organization_owner),
             'category' => Category::make($this->category),
             'categories' => Category::collection($this->categories),
-            // 'likes' => Like::collection($this->likes),
+            'likes' => Like::collection($this->likes),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
             'created_at_explicit' => $this->created_at->format('Y') == date('Y') ? explicitDayMonth($this->created_at->format('Y-m-d H:i:s')) : explicitDate($this->created_at->format('Y-m-d H:i:s')),
