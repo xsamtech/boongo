e9e4de1493703e63193253d06e7d92fc849c4f01
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-09-12 06:37:26 +0100

    Update ReadNotification.php

--- app/Http/Resources/ReadNotification.php
+++ app/Http/Resources/ReadNotification.php
@@ -31,6 +31,10 @@ class ReadNotification extends JsonResource
             'notification' => Notification::make($this->notification),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'created_at_explicit' => $this->created_at->format('Y') == date('Y') ? explicitDayMonth($this->created_at->format('Y-m-d H:i:s')) : explicitDate($this->created_at->format('Y-m-d H:i:s')),
+            'updated_at_explicit' => $this->updated_at->format('Y') == date('Y') ? explicitDayMonth($this->updated_at->format('Y-m-d H:i:s')) : explicitDate($this->updated_at->format('Y-m-d H:i:s')),
+            'created_at_ago' => timeAgo($this->created_at->format('Y-m-d H:i:s')),
+            'updated_at_ago' => timeAgo($this->updated_at->format('Y-m-d H:i:s')),
             'notification_id' => $this->notification_id,
             'user_id' => $this->user_id,
         ];
